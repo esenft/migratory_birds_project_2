c1d700435c4a7ed9435f79a77a097229dd6d59a6
Ellie Senft <esenft@mit.edu>
2026-04-22 16:06:13 +0000

    fifth commit

---
+++ src/services/species_map.py
@@ -0,0 +1,6 @@
+COMMON_TO_SCIENTIFIC = {
+    "Ruby-throated Hummingbird": "Archilochus colubris",
+    "Yellow Warbler": "Setophaga petechia",
+    "Baltimore Oriole": "Icterus galbula",
+    "Red-winged Blackbird": "Agelaius phoeniceus",
+}
